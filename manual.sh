1571c8fb70aee33a3b6b7f09baaf51e5d6a5e64c
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2020-05-11 01:48:00 +0500

    Update manual.sh

--- manual.sh
+++ manual.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/usmanmughalji/build-scripts/master/kernel-environment.sh && chmod +x kernel-environment.sh
+wget https://raw.githubusercontent.com/usmanmughalji/compile-kernel/master/kernel-environment.sh && chmod +x kernel-environment.sh
 
-wget https://raw.githubusercontent.com/usmanmughalji/build-scripts/master/kernel-compile.sh && chmod +x kernel-compile.sh
+wget https://raw.githubusercontent.com/usmanmughalji/compile-kernel/master/kernel-compile.sh && chmod +x kernel-compile.sh
 
-wget https://raw.githubusercontent.com/usmanmughalji/build-scripts/master/upload.sh && chmod +x upload.sh
+wget https://raw.githubusercontent.com/usmanmughalji/compile-kernel/master/upload.sh && chmod +x upload.sh
