825321d8c8c231710ef42b5f49e7b8f440a828d3
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2020-05-14 22:13:30 +0500

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -2,8 +2,8 @@
 
 #Autmate
 
-wget https://raw.githubusercontent.com/usmanmughalji/compile-kernel/master/kernel-environment.sh && chmod +x kernel-environment.sh && ./kernel-environment.sh
+wget https://raw.githubusercontent.com/usmanmughalji/tmp-build/master/kernel-environment.sh && chmod +x kernel-environment.sh && ./kernel-environment.sh
 
-wget https://raw.githubusercontent.com/usmanmughalji/compile-kernel/master/kernel-compile.sh && chmod +x kernel-compile.sh && ./kernel-compile.sh
+wget https://raw.githubusercontent.com/usmanmughalji/tmp-build/master/kernel-compile.sh && chmod +x kernel-compile.sh && ./kernel-compile.sh
 
-wget https://raw.githubusercontent.com/usmanmughalji/compile-kernel/master/upload.sh && chmod +x upload.sh && ./upload.sh
+wget https://raw.githubusercontent.com/usmanmughalji/tmp-build/master/upload.sh && chmod +x upload.sh && ./upload.sh
