64e62eece17f2d4bdf241ea76ed07f418ed8c130
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2020-05-15 22:34:59 +0500

    Update kernel-compile.sh

--- kernel-compile.sh
+++ kernel-compile.sh
@@ -2,7 +2,7 @@
 
 cd
 
-git clone https://github.com/usmanmughalji/tmp-experi.git kernel
+git clone https://github.com/usmanmughalji/android_kernel_xiaomi_msm8937-1.git --single-branch --branch lineage-16.0 kernel
 
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git -b lineage-17.1 kernel/gcc-64 --depth=1
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git -b lineage-17.1 kernel/gcc-32 --depth=1
@@ -12,7 +12,7 @@ cd kernel
 rm -rf out
 
 make O=out clean && make O=out mrproper
-make O=out ARCH=arm64 santoni_defconfig
+make O=out ARCH=arm64 lineageos_santoni_defconfig
 
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
