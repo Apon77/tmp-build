33abd5a19c21f94fe047015bb60d308bcf03f25d
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2020-07-02 20:51:45 +0500

    Update kernel-compile.sh

--- kernel-compile.sh
+++ kernel-compile.sh
@@ -2,7 +2,7 @@
 
 cd
 
-git clone https://github.com/usmanmughalji/non-treble-kernel-source.git kernel
+git clone https://github.com/usmanmughalji/forsantoni.git kernel
 
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git -b lineage-17.1 kernel/gcc-64 --depth=1
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git -b lineage-17.1 kernel/gcc-32 --depth=1
@@ -12,7 +12,7 @@ cd kernel
 rm -rf out
 
 make O=out clean && make O=out mrproper
-make O=out ARCH=arm64 lineageos_santoni_defconfig
+make O=out ARCH=arm64 santoni_defconfig
 
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
